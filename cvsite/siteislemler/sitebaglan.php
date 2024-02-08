fb328715738e5860ceaec86eaebe014b5cf89ee0
Erdinç Öztürk <96015767+erdincoztrk@users.noreply.github.com>
2024-02-08 08:13:51 +0300

    Update sitebaglan.php

--- cvsite/siteislemler/sitebaglan.php
+++ cvsite/siteislemler/sitebaglan.php
@@ -1,7 +1,7 @@
 <?php 
 
 try {
-	$db=new PDO("mysql:host=localhost;dbname=cvsite;charset=utf8",'root','yalova771907');
+	$db=new PDO("mysql:host=localhost;dbname=cvsite;charset=utf8",'root','');
 	
 } catch (PDOException $e) {
 	echo $e -> getMessage();
